06700ffc019d0f752b09c4e4d5594560a9c95840
João Paulo Canário <jopacanario@gmail.com>
2017-08-21 15:23:59 -0300

    Remove data before 2015

--- research/develop/2017-08-20-joaopcanario-companies-analisys.py
+++ research/develop/2017-08-20-joaopcanario-companies-analisys.py
@@ -5,7 +5,7 @@
 # 
 # This notebook provides an exploratory analysis on companies with the same name but different CNPJ's. On this analysis it'll be tried to know more about their existence through an exploratory analysis, and possibly get more insights for new irregularities.
 
-# In[1]:
+# In[ ]:
 
 from serenata_toolbox.datasets import Datasets
 from pylab import rcParams
@@ -31,11 +31,11 @@ datasets.downloader.download('2017-07-04-reimbursements.xz')
 datasets.downloader.download('2017-05-21-companies-no-geolocation.xz')
 
 
-# In[2]:
+# In[ ]:
 
 # Loading companies dataset
-CP_DTYPE =dict(cnpj=np.str, name=np.str, main_activity_code='category',
-               legal_entity='category', main_activity='category',
+CP_DTYPE =dict(cnpj=np.str, name=np.str,
+               main_activity_code='category', legal_entity='category',
                partner_1_name=np.str, partner_1_qualification='category',
                partner_2_name=np.str, partner_2_qualification='category',
                situation='category', state='category',
@@ -49,7 +49,7 @@ companies = pd.read_csv('../data/2017-05-21-companies-no-geolocation.xz',
 # companies = companies.dropna(axis=[0, 1], how='all').dropna(axis=1, thresh=30000)
 companies['cnpj'].replace(to_replace='[^0-9]+', value='', inplace=True, regex=True)
 
-c = companies[['cnpj', 'last_updated', 'legal_entity', 'main_activity', 'main_activity_code',
+c = companies[['cnpj', 'last_updated', 'legal_entity', 'main_activity_code',
                'name', 'opening', 'partner_1_name', 'partner_1_qualification',
                'partner_2_name', 'partner_2_qualification',
                'situation', 'situation_date', 'state', 'status', 'type']]
@@ -59,7 +59,7 @@ c.columns.values[0] = 'cnpj_cpf'
 c.head(5)
 
 
-# In[3]:
+# In[ ]:
 
 # Loading reimbursments dataset
 R_DTYPE =dict(cnpj_cpf=np.str, year=np.int16, month=np.int16,
@@ -80,12 +80,49 @@ r = reimbursements[['year', 'month', 'total_net_value', 'party',
 r.head(10)
 
 
-# In[4]:
+# In[ ]:
 
 # r.groupby(['supplier', 'congressperson_name', 'year'])['total_net_value'].sum().sort_values(ascending=False).head(20)
 filtered_c = c[c['cnpj_cpf'].isin(r.cnpj_cpf.unique())]
-r = r.merge(filtered_c, on='cnpj_cpf', how='left')
-r.head(10)
+data = r.merge(filtered_c, on='cnpj_cpf', how='left')
+data = data[data.year >= 2016]
+
+data.head(10)
+
+
+# In[ ]:
+
+# count objects with invalid main_activity_code
+d = dict()
+
+invalid_main_activity = "00.00-0-00"
+data_len = len(data)
+
+d['valid'] = len(data[data.main_activity_code_x != invalid_main_activity]) / data_len * 100
+d['invalid'] = len(data[data.main_activity_code_x == invalid_main_activity]) / data_len * 100
+
+s = pd.Series(d)
+s.plot(kind='pie', autopct='%.2f')
+plt.title('Number of valid and invalid main_activity_code in dataset')
+
+
+# In[ ]:
+
+# remove items with invalid main_activity_code
+data = data[data.main_activity_code_x != "00.00-0-00"]
+data.head(5)
+
+
+# In[ ]:
+
+# remove items previous than 2015
+data = data[data['year'] >= 2015]
+data.head(5)
+
+
+# In[ ]:
+
+data.shape
 
 
 # In[ ]:
